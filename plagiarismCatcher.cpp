e373a5cf3ab134c8da727363df84bf48db0ccca0
Colby Janecka <colbyjanecka@aol.com>
2019-05-07 13:48:45 -0500

    added getFiles helper function
    getFiles function

--- plagiarismCatcher.cpp
+++ plagiarismCatcher.cpp
@@ -7,6 +7,31 @@
 
 using namespace std;
 
-int main () {
+int getFiles(string dir, vector<string> &files);
 
+int main(){ }
+
+
+/**
+   getFiles
+
+   function that gets the file names from the directory and
+   stores them in the vector supplied as argument 2.
+
+ **/
+int getFiles(string dir, vector<string> &files)
+{
+	DIR * openDir;
+	struct dirent * readDir;
+
+	if ((openDir = opendir(dir.c_str())) == NULL) {
+		cout << "ERROR (" << errno << ") opening " << dir << endl;
+		return (errno);
+	}
+
+	while ((readDir = readdir(dp)) != NULL) {
+		files.push_back(string(readDir->d_name));
+	}
+	closedir(openDir);
+	return (0);
 }

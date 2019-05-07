d6a5e490789d741176acff51d3ce2bba3e142450
coreykarnei <31517198+coreykarnei@users.noreply.github.com>
2019-05-07 13:52:22 -0500

    Add files via upload

--- hash.cpp
+++ hash.cpp
@@ -8,7 +8,7 @@ const int SIZE = 1000000;
 
 hashTable::hashTable() {
     table = new hashNode*[SIZE];
-    for(int i =0; i<SIZE; i++){
+    for(int i =0; i<SIZE; i++){                                 //constructs table with contents = NULL
         table[i] = NULL;
     }
 }
@@ -16,14 +16,14 @@ hashTable::hashTable() {
 
 void hashTable::addNode(int hashVal, int index) {
     hashNode* cur = table[hashVal];
-    while(cur != NULL){
+    while(cur != NULL){                                         //do nothing if index already in correct spot
         if(cur->getFileIndex == index){
             return;
         }
         cur = cur->getNext;
     }
     hashNode* insert = new hashNode(index);
-    insert->setNext(table[hashVal]);
+    insert->setNext(table[hashVal]);                            //add node at specified hashVal
     table[hashVal] = insert;
 }
 
@@ -31,14 +31,14 @@ void hashTable::addNode(int hashVal, int index) {
 
 
 hashNode* hashTable::getPointer(int hashVal) {
-    return table[hashVal];
+    return table[hashVal];                                      //return pointer to hashVal
 }
 
 
 
 void hashTable::deleteList(int hashVal) {
     hashNode* cur = table[hashVal];
-    while(cur != NULL){
+    while(cur != NULL){                                         //delete list till pointing to NULL
         hashNode *del = cur;
         cur = cur->getNext();
         delete (del);
@@ -48,7 +48,7 @@ void hashTable::deleteList(int hashVal) {
 void hashTable::makeEmpty() {
     int i = 0;
     while(i<SIZE){
-        deleteList(i);
+        deleteList(i);                                          //delete it all!
         i++;
     }
 }
